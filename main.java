2baaf76f4e59b17a4fca657f29cfddb328a3e2bf
Lina <l.alhamoud@uzuner-consulting.com>
2021-01-27 22:16:52 +0100

    fisrt version

---
+++ main.java
@@ -0,0 +1,45 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("\t\t ##");
+        System.out.println("\t\t####");
+        System.out.println("\t   ######");
+        System.out.println("\t ##########");
+        System.out.println("\t############");
+        System.out.println("   ##############");
+        System.out.println("\t\t ##");
+        System.out.println("\t\t ##");
+        System.out.println("\n\n\n");
+
+        System.out.println("#\t\t#\t#########\t#\t\t\t#\t\t\t#########");
+        System.out.println("#\t\t#\t#\t\t#\t#\t\t\t#\t\t\t#\t\t#");
+        System.out.println("#\t\t#\t#\t\t#\t#\t\t\t#\t\t\t#\t\t#");
+        System.out.println("#\t\t#\t#\t\t#\t#\t\t\t#\t\t\t#\t\t#");
+        System.out.println("#########\t#########\t#\t\t\t#\t\t\t#\t\t#");
+        System.out.println("#\t\t#\t#\t\t#\t#\t\t\t#\t\t\t#\t\t#");
+        System.out.println("#\t\t#\t#\t\t#\t#\t\t\t#\t\t\t#\t\t#");
+        System.out.println("#\t\t#\t#\t\t#\t#\t\t\t#\t\t\t#\t\t#");
+        System.out.println("#\t\t#\t#\t\t#\t#\t\t\t#\t\t\t#\t\t#");
+        System.out.println("#\t\t#\t#\t\t#\t#########\t#########\t#########");
+
+        System.out.println("\n\n");
+        System.out.println("#\t\t\t\t  #\t\t########\t#\t\t\t#########");
+        System.out.println(" #\t\t\t\t #\t\t#\t\t\t#\t\t\t\t#");
+        System.out.println("  #\t\t\t\t#\t\t#\t\t\t#\t\t\t\t#");
+        System.out.println("   #\t\t   #\t\t#\t\t\t#\t\t\t\t#");
+        System.out.println("    #\t\t  #\t\t\t#####\t\t#\t\t\t\t#");
+        System.out.println("\t #   #   #\t\t\t#\t\t\t#\t\t\t\t#");
+        System.out.println("\t  # # # #\t\t\t#\t\t\t#\t\t\t\t#");
+        System.out.println("\t   #   #\t\t\t########\t#########\t\t#");
+
+
+
+
+
+
+
+
+    }
+}
+
+        }
